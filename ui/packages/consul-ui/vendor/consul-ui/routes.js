cf4aae174bfef706c6f12f818d44f95c849df71f
Michael Klein <michael@firstiwaslike.com>
2022-07-18 15:32:36 +0200

    Reflect change to searchproperty in all app/sub-app route configs
    We need to change the configuration in all route configs to make this
    work

--- ui/packages/consul-ui/vendor/consul-ui/routes.js
+++ ui/packages/consul-ui/vendor/consul-ui/routes.js
@@ -46,7 +46,7 @@
               kind: 'kind',
               searchproperty: {
                 as: 'searchproperty',
-                empty: [['Partition', 'Name', 'Tags', 'PeerName']],
+                empty: ['Partition', 'Name', 'Tags'],
               },
               search: {
                 as: 'filter',
@@ -69,7 +69,14 @@
                 searchproperty: {
                   as: 'searchproperty',
                   empty: [
-                    ['Name', 'Node', 'Tags', 'ID', 'Address', 'Port', 'Service.Meta', 'Node.Meta'],
+                    'Name',
+                    'Node',
+                    'Tags',
+                    'ID',
+                    'Address',
+                    'Port',
+                    'Service.Meta',
+                    'Node.Meta',
                   ],
                 },
                 search: {
@@ -89,7 +96,7 @@
                   access: 'access',
                   searchproperty: {
                     as: 'searchproperty',
-                    empty: [['SourceName', 'DestinationName']],
+                    empty: ['SourceName', 'DestinationName'],
                   },
                   search: {
                     as: 'filter',
@@ -119,7 +126,7 @@
                 instance: 'instance',
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['Name', 'Tags']],
+                  empty: ['Name', 'Tags'],
                 },
                 search: {
                   as: 'filter',
@@ -136,7 +143,7 @@
                 instance: 'instance',
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['Name', 'Tags']],
+                  empty: ['Name', 'Tags'],
                 },
                 search: {
                   as: 'filter',
@@ -166,7 +173,7 @@
                 check: 'check',
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['Name', 'Node', 'CheckID', 'Notes', 'Output', 'ServiceTags']],
+                  empty: ['Name', 'Node', 'CheckID', 'Notes', 'Output', 'ServiceTags'],
                 },
                 search: {
                   as: 'filter',
@@ -186,7 +193,7 @@
                 },
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['DestinationName', 'LocalBindAddress', 'LocalBindPort']],
+                  empty: ['DestinationName', 'LocalBindAddress', 'LocalBindPort'],
                 },
               },
             },
@@ -215,7 +222,7 @@
               status: 'status',
               searchproperty: {
                 as: 'searchproperty',
-                empty: [['Node', 'Address', 'Meta', 'PeerName']],
+                empty: ['Node', 'Address', 'Meta'],
               },
               search: {
                 as: 'filter',
@@ -236,7 +243,7 @@
                 check: 'check',
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['Name', 'Service', 'CheckID', 'Notes', 'Output', 'ServiceTags']],
+                  empty: ['Name', 'Service', 'CheckID', 'Notes', 'Output', 'ServiceTags'],
                 },
                 search: {
                   as: 'filter',
@@ -254,7 +261,7 @@
                 source: 'source',
                 searchproperty: {
                   as: 'searchproperty',
-                  empty: [['Name', 'Tags', 'ID', 'Address', 'Port', 'Service.Meta']],
+                  empty: ['Name', 'Tags', 'ID', 'Address', 'Port', 'Service.Meta'],
                 },
                 search: {
                   as: 'filter',
@@ -281,7 +288,7 @@
               access: 'access',
               searchproperty: {
                 as: 'searchproperty',
-                empty: [['SourceName', 'DestinationName']],
+                empty: ['SourceName', 'DestinationName'],
               },
               search: {
                 as: 'filter',
