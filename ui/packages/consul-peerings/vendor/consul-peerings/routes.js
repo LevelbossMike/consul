474de792bb257a5ebc3bf5fa9629ff6821e2919a
Michael Klein <michael@firstiwaslike.com>
2022-07-18 15:33:40 +0200

    Update peerings app to add optional searchProperty nodes/services-route

--- ui/packages/consul-peerings/vendor/consul-peerings/routes.js
+++ ui/packages/consul-peerings/vendor/consul-peerings/routes.js
@@ -1,40 +1,88 @@
-(routes => routes({
-  dc: {
-    peers: {
-      _options: { 
-        path: '/peers'
-      },
-      index: {
-        _options: {
-          path: '/',
-          queryParams: {
-            sortBy: 'sort',
-            state: 'state',
-            searchproperty: {
-              as: 'searchproperty',
-              empty: [['Name']],
-            },
-            search: {
-              as: 'filter',
-              replace: true,
+((routes) =>
+  routes({
+    dc: {
+      services: {
+        _options: { path: "/services" },
+        index: {
+          _options: {
+            path: "/",
+            queryParams: {
+              sortBy: "sort",
+              status: "status",
+              source: "source",
+              kind: "kind",
+              searchproperty: {
+                as: "searchproperty",
+                empty: ["PeerName"],
+              },
+              search: {
+                as: "filter",
+                replace: true,
+              },
             },
           },
         },
       },
-      edit: {
-        _options: {
-          path: '/:name'
-        },
-        addresses: {
+      nodes: {
+        _options: { path: "/nodes" },
+        index: {
           _options: {
-            path: '/addresses',
+            path: "",
+            queryParams: {
+              sortBy: "sort",
+              status: "status",
+              searchproperty: {
+                as: "searchproperty",
+                empty: ["PeerName"],
+              },
+              search: {
+                as: "filter",
+                replace: true,
+              },
+            },
+          },
+        },
+      },
+      peers: {
+        _options: {
+          path: "/peers",
+        },
+        index: {
+          _options: {
+            path: "/",
+            queryParams: {
+              sortBy: "sort",
+              state: "state",
+              searchproperty: {
+                as: "searchproperty",
+                empty: ["Name"],
+              },
+              search: {
+                as: "filter",
+                replace: true,
+              },
+            },
+          },
+        },
+        edit: {
+          _options: {
+            path: "/:name",
+          },
+          addresses: {
+            _options: {
+              path: "/addresses",
+            },
           },
         },
       },
     },
-  },
-}))(
-  (json, data = (typeof document !== 'undefined' ? document.currentScript.dataset : module.exports)) => {
+  }))(
+  (
+    json,
+    data = typeof document !== "undefined"
+      ? document.currentScript.dataset
+      : module.exports
+  ) => {
     data[`routes`] = JSON.stringify(json);
   }
 );
