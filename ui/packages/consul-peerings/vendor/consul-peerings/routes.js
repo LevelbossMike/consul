701a5fcc09d42f367bb3bdcdba3682aad42ea355
Michael Klein <michael@firstiwaslike.com>
2022-07-18 15:47:58 +0200

    Cleanup peerings route-config
    We don't need to copy-paste entire route configuration we can only
    add the parts we want to customize as configs are getting merged
    internally.

--- ui/packages/consul-peerings/vendor/consul-peerings/routes.js
+++ ui/packages/consul-peerings/vendor/consul-peerings/routes.js
@@ -2,42 +2,22 @@
   routes({
     dc: {
       services: {
-        _options: { path: "/services" },
         index: {
           _options: {
-            path: "/",
             queryParams: {
-              sortBy: "sort",
-              status: "status",
-              source: "source",
-              kind: "kind",
               searchproperty: {
-                as: "searchproperty",
                 empty: ["PeerName"],
-              },
-              search: {
-                as: "filter",
-                replace: true,
               },
             },
           },
         },
       },
       nodes: {
-        _options: { path: "/nodes" },
         index: {
           _options: {
-            path: "",
             queryParams: {
-              sortBy: "sort",
-              status: "status",
               searchproperty: {
-                as: "searchproperty",
                 empty: ["PeerName"],
-              },
-              search: {
-                as: "filter",
-                replace: true,
               },
             },
           },
